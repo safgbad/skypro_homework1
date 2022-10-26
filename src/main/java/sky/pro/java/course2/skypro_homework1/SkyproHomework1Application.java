120eafe667aba3583b811e82b38de0615f3e674f
Artem Nazarov <art.nazar.11@gmail.com>
2022-10-27 02:17:24 +0300

    rollback

--- src/main/java/sky/pro/java/course2/skypro_homework1/SkyproHomework1Application.java
+++ src/main/java/sky/pro/java/course2/skypro_homework1/SkyproHomework1Application.java
@@ -1,4 +1,4 @@
-package sky.pro.java.course2.skypro_homework1.hw15;
+package sky.pro.java.course2.skypro_homework1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
