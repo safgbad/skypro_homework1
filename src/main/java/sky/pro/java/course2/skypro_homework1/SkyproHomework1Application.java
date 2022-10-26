30a44150a84a6aec59d67bcebba069d80c9eb370
Artem Nazarov <art.nazar.11@gmail.com>
2022-10-27 01:38:42 +0300

    Initial commit

---
+++ src/main/java/sky/pro/java/course2/skypro_homework1/SkyproHomework1Application.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.skypro_homework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHomework1Application.class, args);
+    }
+
+}
